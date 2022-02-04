edfba577251cb1d337771c0967af0b55708792a2
cmclean5404 <12002+cmclean5404@users.noreply.github.tamu.edu>
2022-02-03 18:34:11 -0600

    Adding new attributes and delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,9 +48,13 @@ class BooksController < ApplicationController
   end
 
   # DELETE /books/1 or /books/1.json
+  def delete
+	set_book
+  end
+  
   def destroy
-    @book.destroy
-
+	@book.destroy
+	
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
       format.json { head :no_content }
@@ -65,6 +69,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title)
+      params.require(:book).permit(:title, :author, :price, :date)
     end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,33 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'J.K Rowling')
+  end
+
+  it 'is valid with valid attributes (author)' do
+    expect(subject).to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', price: 20)
+  end
+
+  it 'is valid with valid attributes (price)' do
+    expect(subject).to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', date: Date.new(1997,6,26))
+  end
+
+  it 'is valid with valid attributes (date)' do
+    expect(subject).to be_valid
+  end
+end
